b518a28d732ef17bfd895d512127cca86833090b
sernan <sernan0906@gmail.com>
2026-01-07 11:57:34 +0900

    feat: 테스트용 랜덤 주소 생성 함수 구현

--- Main.c
+++ Main.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
-
+#include "random_address.h"
 int main(void) {
-	printf("Hello C");
+	while(1){
+		uint32_t addr = make_random_address();
+    	printf("랜덤 주소: 0x%08X\n", addr);
+	}
 	return 0;
 }
---
+++ random_address.h
@@ -0,0 +1,8 @@
+#ifndef RANDOM_ADDRESS_H
+#define RANDOM_ADDRESS_H
+
+#include <stdint.h>
+
+uint32_t make_random_address(void);
+
+#endif
